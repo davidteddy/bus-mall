6469d5de9ef481fd0331abbd9e6da43cc0421f31
David Teddy <davidteddy@outlook.com>
2017-01-19 07:58:30 -0800

    Chart is done, Brandon collaberated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bus-mall
+
+# Brandon Little and I collaborated together on this projected. He helped me get started and we both worked together to get the listeners to work and I created the graph and he got it to work. 
--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var num2 = 0;
 var num3 = 0;
 var imgEl1, imgEl2, imgEl3;
 var showenImg1, showenImg2, showenImg3;
-
 var selectEl = document.getElementById('setting-img');
-
+var chartData = [];
+//image object constructor
 function ProductImg(imgName, filePath, id){
   this.imgName = imgName;
   this.filePath = filePath;
@@ -16,7 +16,7 @@ function ProductImg(imgName, filePath, id){
   this.clickCount = 0;
   this.timeShown = 0;
 }
-
+//initializing the images into the constructor
 var bag = new ProductImg('bag', 'img/bag.jpg', 'bag');
 var banana = new ProductImg('banana', 'img/banana.jpg', 'banana');
 var bathroom = new ProductImg('bathroom', 'img/bathroom.jpg', 'bathroom');
@@ -37,38 +37,34 @@ var unicorn = new ProductImg ('unicorn', 'img/unicorn.jpg', 'unicorn');
 var usb = new ProductImg ('usb', 'img/usb.gif', 'usb');
 var waterCan = new ProductImg ('water-can', 'img/water-can.jpg', 'waterCan');
 var wineGlass = new ProductImg ('wine-glass', 'img/wine-glass.jpg', 'wineGlass');
-
+//pushing the image objects into an array
 var imageArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
-
+//function to generate three non-repeating random numbers and store them
 var imgSrc = function() {
   //chooses a random number, so long as that number was not in the selectionNumbers array and sets it to a given position in the array
   do {
-    num1 = Math.floor(Math.random() * (imageArray.length));
+    num1 = Math.floor(Math.random() * (imageArray.length + 1));
   } while (selectionNumbers.includes(num1) || num1 === num2 || num1 === num3 || num1 === 20);
-
   do {
-    num2 = Math.floor(Math.random() * (imageArray.length));
+    num2 = Math.floor(Math.random() * (imageArray.length + 1));
   } while (selectionNumbers.includes(num2) || num2 === num1 || num2 === num3 || num2 === 20);
-
   do {
-    num3 = Math.floor(Math.random() * (imageArray.length));
+    num3 = Math.floor(Math.random() * (imageArray.length + 1));
   } while (selectionNumbers.includes(num3) || num3 === num1 || num3 === num2 || num3 === 20);
-
   selectionNumbers[0] = num1;
   selectionNumbers[1] = num2;
   selectionNumbers[2] = num3;
-
   //log array to confirm non-repeating numbers are generated each time function is run
   console.log(selectionNumbers);
 };
-
+//function to remove the images from the page (currently not in use)
 var imgRem = function(){
   var inputId = document.getElementById('setting-img');
-
   if (inputId !== null){
     inputId.remove();
   }
 };
+//function to create the html elements for the image render
 var createRendItem = function(){
   var sectionEl = document.createElement('section');
   sectionEl.setAttribute('id', 'funk');
@@ -85,96 +81,89 @@ var createRendItem = function(){
   selectEl.appendChild(sectionEl);
 };
 createRendItem();
+//function to render the current set of randomized images to the page
 var rendImg = function (){
-  // imgRem();
   imgSrc();
   showenImg1 = imageArray[selectionNumbers[0]];
   showenImg2 = imageArray[selectionNumbers[1]];
   showenImg3 = imageArray[selectionNumbers[2]];
-    // for(var i = 0; i < selectionNumbers.length; i++){
-    // imgEl.setAttribute('type', 'image');
   imgEl1.setAttribute('src', imageArray[selectionNumbers[0]].filePath);
   imgEl2.setAttribute('src', imageArray[selectionNumbers[1]].filePath);
   imgEl3.setAttribute('src', imageArray[selectionNumbers[2]].filePath);
-
-  // imgEl1.setAttribute('id', imageArray[selectionNumbers[0]].clickCount);
-  // imgEl2.setAttribute('id', imageArray[selectionNumbers[1]].clickCount);
-  // imgEl3.setAttribute('id', imageArray[selectionNumbers[2]].clickCount);
-  // imageArray[selectionNumbers[i]].timeShown++;
-  // }
 };
-
 rendImg();
-
+//event listeners for each of the three images on the page
 imgEl1.addEventListener('click',imgOne, false);
 function imgOne(){
-  imgSrc();
   showenImg1.clickCount++;
-  cycleCount++;
   console.log(showenImg1.clickCount);
 }
-
 imgEl2.addEventListener('click',imgTwo, false);
 function imgTwo(){
-  imgSrc();
   showenImg2.clickCount++;
-  cycleCount++;
   console.log(showenImg2.clickCount);
 }
-
 imgEl3.addEventListener('click',imgThree, false);
 function imgThree(){
-  imgSrc();
   showenImg3.clickCount++;
-  cycleCount++;
   console.log(showenImg3.clickCount);
 }
 //listener for when an image is selected
 var choices = document.getElementById('setting-img');
-
-choices.onclick = function() {
-  // console.log(event);
-  // console.log(event.target.imgName);
-  if (cycleCount < 25) {
+choices.addEventListener('click', function clickListener() {
+  if (cycleCount < 5) {
     for (var i = 0; i < imageArray.length; i++) {
       if (imageArray[i].imgName == this.imgName) {
         imageArray[i].clickCount++;
-
       }
     }
     rendImg();
     cycleCount++;
-    // console.log(cycleCount);
   } else {
-    imgRem();
-    var completionMessage = document.createElement('p');
-    completionMessage.textContent = 'Your finished';
-    selectEl.appendChild(completionMessage);
+    //build the chart data
+    for (var index = 0; index < imageArray.length; index++) {
+      var clickedData = imageArray[index].clickCount;
+      chartData[index] = clickedData;
+    }
+    console.log(chartData);
+    buildChart();
+    imgEl1.removeEventListener('click',imgOne);
+    imgEl2.removeEventListener('click',imgTwo);
+    imgEl3.removeEventListener('click',imgThree);
+    choices.removeEventListener('click',clickListener);
   }
-  //choices.addEventListner('click');
-};
-
+}, false);
 var context = document.getElementById('charting').getContext('2d');
-
-var chartColors = ['blue', 'red', 'green', 'yellow', 'purple', 'orange', 'black', 'white', 'pink', 'teal', 'brown', 'lime-green', 'blue', 'red', 'orange', 'green', 'purple', 'black', 'yellow', 'brown'];
-var chartOptions = {
-  responsive: true,
-  scales:{
-    yAxes:[{
-      ticks:{
-        beginAtZero: true
-      }
-    }]
+//variables and options for building the results chart on the page
+var buildChart = function() {
+  var renderChart = document.getElementById('charting');
+  //build list of names for labels
+  var chartLabels = [];
+  for (var index = 0; index < imageArray.length; index++) {
+    var labelsData = imageArray[index].imgName;
+    chartLabels[index] = labelsData;
   }
+  var chartColors = ['red', 'yellow', 'blue', 'green', 'purple', 'red', 'yellow', 'blue', 'green', 'purple', 'red', 'yellow', 'blue', 'green', 'purple', 'red', 'yellow', 'blue', 'green', 'purple'];
+  var chartOptions = {
+    responsive: false,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  var imageResultsChart = new Chart(context, {
+    type: 'bar',
+    data: {
+      labels: chartLabels,
+      datasets: [{
+        label: '# of votes for each color',
+        data: chartData,
+        backgroundColor: chartColors
+      }]
+    },
+    options: chartOptions
+  });
 };
-
-var awesomeChart = new Chart(context, {
-  type:'bar',
-  data:{
-    labels:['red', 'green', 'blue', 'white'],
-    datasets:[{
-      label:'Photo Vote',
-      data:imageArray[0].clickCount
-    }]
-  }
-});
